8620d50c3a0f6b55860bda02d45a1fc762b21634
alexmy <alexmy@alexmy-ThinkPad-P51>
2019-07-06 18:26:17 -0400

    initial commit KO

---
+++ src/main/java/org/lisapark/koctopus/compute/source/RTCSource.java
@@ -0,0 +1,146 @@
+/* 
+ * Copyright (C) 2013 Lisa Park, Inc. (www.lisa-park.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.lisapark.koctopus.compute.source;
+
+import com.google.common.collect.Maps;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+import org.lisapark.koctopus.core.Output;
+import org.lisapark.koctopus.core.Persistable;
+import org.lisapark.koctopus.core.ValidationException;
+import org.lisapark.koctopus.core.event.Event;
+import org.lisapark.koctopus.core.event.EventType;
+import org.lisapark.koctopus.core.parameter.Parameter;
+import org.lisapark.koctopus.core.parameter.Parameter.Builder;
+import org.lisapark.koctopus.core.runtime.ProcessingRuntime;
+import org.lisapark.koctopus.core.source.external.CompiledExternalSource;
+import org.lisapark.koctopus.core.source.external.ExternalSource;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author alex (alexmy@lisa-park.com)
+ */
+@Persistable
+public class RTCSource extends ExternalSource {
+
+    private static final String DEFAULT_NAME = "RTC Source";
+    private static final String DEFAULT_DESCRIPTION = "Run Time Container Source Processor. Initiates Chained Model Work Flow.";
+    private static final int RTC_SIGNAL_NAME_PARAMETER_ID = 1;
+    private static final int RTC_SIGNAL_VALUE_PARAMETER_ID = 2;
+
+    public RTCSource(UUID id, String name, String description) {
+        super(id, name, description);
+    }
+
+    private RTCSource(UUID id, RTCSource copyFromSource) {
+        super(id, copyFromSource);
+    }
+
+    public RTCSource(RTCSource copyFromSource) {
+        super(copyFromSource);
+    }
+
+    public String getStartSignalName() {
+        return getParameter(RTC_SIGNAL_NAME_PARAMETER_ID).getValueAsString();
+    }
+
+    public Boolean getStartSignalValue() {
+        return (Boolean) getParameter(RTC_SIGNAL_VALUE_PARAMETER_ID).getValue();
+    }
+
+    @Override
+    public RTCSource copyOf() {
+        return new RTCSource(this);
+    }
+
+    @Override
+    public RTCSource newInstance() {
+        UUID sourceId = UUID.randomUUID();
+        return new RTCSource(sourceId, this);
+    }
+
+    public static RTCSource newTemplate() {
+        UUID sourceId = UUID.randomUUID();
+
+        RTCSource rtcSource = new RTCSource(sourceId, "RTC Source", "Generates run token to start all Octopus model's"
+                + " containers that are connected to the source.");
+        rtcSource.setOutput(Output.outputWithId(1).setName("Output data"));
+
+        rtcSource.addParameter(Parameter.stringParameterWithIdAndName(RTC_SIGNAL_NAME_PARAMETER_ID, "Run signal name:")
+                .description("Run signal/token name.")
+                .defaultValue("start")
+                .required(true));
+
+        rtcSource.addParameter(Parameter.booleanParameterWithIdAndName(RTC_SIGNAL_VALUE_PARAMETER_ID, "Run signal value:")
+                .description("Run signal value.")
+//                .defaultValue(true)
+                .required(true));
+
+        return rtcSource;
+    }
+
+    @Override
+    public CompiledExternalSource compile() throws ValidationException {
+        return new CompiledRedisSource(copyOf());
+    }
+
+    class CompiledRedisSource implements CompiledExternalSource {
+
+        protected final Logger logger = Logger.getLogger(CompiledRedisSource.class.getName());
+        protected final RTCSource source;
+        protected volatile boolean running;
+        protected Thread thread;
+        private long SLIEEP_TIME = 100L;
+
+        public CompiledRedisSource(RTCSource source) {
+            this.source = source;
+        }
+
+        @Override
+        public void startProcessingEvents(ProcessingRuntime runtime) {
+            
+            thread = Thread.currentThread();
+            running = true;
+            
+            int numberEventsCreated = 0;
+
+            Map attributeData = Maps.newHashMap();
+            attributeData.put(this.source.getStartSignalName(), this.source.getStartSignalValue());
+
+            while (!thread.isInterrupted() && running && numberEventsCreated < 1) {
+                Event e = new Event(attributeData);
+
+                runtime.sendEventFromSource(e, source);
+                
+                numberEventsCreated++;
+                try {
+                    Thread.sleep(SLIEEP_TIME);
+                } catch (InterruptedException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }            
+        }
+
+        @Override
+        public void stopProcessingEvents() {
+            this.running = false;
+        }
+    }
+}
---
+++ src/main/java/org/lisapark/koctopus/compute/source/TestRandomBinarySource.java
@@ -0,0 +1,164 @@
+/* 
+ * Copyright (C) 2013 Lisa Park, Inc. (www.lisa-park.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.lisapark.koctopus.compute.source;
+
+import com.google.common.collect.Maps;
+import org.lisapark.koctopus.core.Output;
+import org.lisapark.koctopus.core.Persistable;
+import org.lisapark.koctopus.core.ValidationException;
+import org.lisapark.koctopus.core.event.Attribute;
+import org.lisapark.koctopus.core.event.Event;
+import org.lisapark.koctopus.core.event.EventType;
+import org.lisapark.koctopus.core.parameter.Constraints;
+import org.lisapark.koctopus.core.parameter.Parameter;
+import org.lisapark.koctopus.core.runtime.ProcessingRuntime;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.lisapark.koctopus.core.source.external.CompiledExternalSource;
+import org.lisapark.koctopus.core.source.external.ExternalSource;
+import org.openide.util.Exceptions;
+
+/**
+ * @author dave sinclair(david.sinclair@lisa-park.com)
+ */
+@Persistable
+public class TestRandomBinarySource extends ExternalSource {
+
+    private static final String DEFAULT_NAME = "Test Random Binary";
+    private static final String DEFAULT_DESCRIPTION = "Generate random binary source "
+            + "data according to the provided attribute list.";
+
+    private static final int NUMBER_OF_EVENTS_PARAMETER_ID = 1;
+    private static final int BREAK_POINT_PARAMETER_ID = 2;
+
+    public TestRandomBinarySource(UUID id, String name, String description) {
+        super(id, name, description);
+    }
+
+    private TestRandomBinarySource(UUID id, TestRandomBinarySource copyFromSource) {
+        super(id, copyFromSource);
+    }
+
+    public TestRandomBinarySource(TestRandomBinarySource copyFromSource) {
+        super(copyFromSource);
+    }
+
+    public Integer getNumberOfEvents() {
+        return getParameter(NUMBER_OF_EVENTS_PARAMETER_ID).getValueAsInteger();
+    }
+
+    
+    public Double getBreakPoint() {
+        return (Double) getParameter(BREAK_POINT_PARAMETER_ID).getValue();
+    }
+
+    @Override
+    public TestRandomBinarySource copyOf() {
+        return new TestRandomBinarySource(this);
+    }
+
+    @Override
+    public TestRandomBinarySource newInstance() {
+        UUID sourceId = UUID.randomUUID();
+        return new TestRandomBinarySource(sourceId, this);
+    }
+
+    public static TestRandomBinarySource newTemplate() {
+        UUID sourceId = UUID.randomUUID();
+
+        TestRandomBinarySource testSource = new TestRandomBinarySource(sourceId, DEFAULT_NAME, DEFAULT_DESCRIPTION);
+        testSource.setOutput(Output.outputWithId(1).setName("Output"));
+        testSource.addParameter(
+                Parameter.integerParameterWithIdAndName(NUMBER_OF_EVENTS_PARAMETER_ID, "Number of Events").
+                        description("Number of test events to generate.").
+                        defaultValue(10).
+                        constraint(Constraints.integerConstraintWithMinimumAndMessage(1,
+                        "Number of events has to be greater than zero.")));
+        
+        testSource.addParameter(
+                Parameter.doubleParameterWithIdAndName(BREAK_POINT_PARAMETER_ID, "Break Point").
+                        description("Number in the interval [0, 1]. If random generated namber"
+                                + " is greater than this number output is 1, otherwise - 0.").
+                        defaultValue(.5).
+                        required(true));
+        
+        return testSource;
+    }
+
+    @Override
+    public CompiledExternalSource compile() throws ValidationException {
+        return new CompiledTestSource(copyOf());
+    }
+
+    static class CompiledTestSource implements CompiledExternalSource {
+
+        private final TestRandomBinarySource source;
+
+        /**
+         * Running is declared volatile because it may be access my different threads
+         */
+        private volatile boolean running;
+        private final long SLIEEP_TIME = 1L;
+
+        public CompiledTestSource(TestRandomBinarySource source) {
+            this.source = source;
+        }
+
+        @Override
+        public void startProcessingEvents(ProcessingRuntime runtime) {
+            Thread thread = Thread.currentThread();
+            running = true;
+
+            EventType eventType = source.getOutput().getEventType();
+            List<Attribute> attributes = eventType.getAttributes();
+            int numberEventsCreated = 0;
+
+            while (!thread.isInterrupted() && running && numberEventsCreated < source.getNumberOfEvents()) {
+                
+                int x = (Math.random() < source.getBreakPoint())? 0 : 1;
+                Event e = createEvent(attributes, x);
+
+                runtime.sendEventFromSource(e, source);
+                
+                numberEventsCreated++;
+                
+                try {
+                    Thread.sleep(SLIEEP_TIME);
+                } catch (InterruptedException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }
+
+        private Event createEvent(List<Attribute> attributes, int random) {
+            Map<String, Object> attributeData = Maps.newHashMap();
+
+            for (Attribute attribute : attributes) {
+                attributeData.put(attribute.getName(), attribute.createSampleData(random));
+            }
+
+            return new Event(attributeData);
+        }
+
+        @Override
+        public void stopProcessingEvents() {
+            running = false;
+        }
+    }
+}
